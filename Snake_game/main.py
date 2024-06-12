1960ac7ed99cd0733c1ba54cbd787d788fccf90e
Monde Nel <Mondenel1996@gmail.com>
2024-06-12 14:55:58 +0200

    Created a class of snake, and added some methods that will instruct the movement of the snake

---
+++ Snake_game/main.py
@@ -0,0 +1,56 @@
+import turtle
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+        self.direction = "stop"
+
+    def create_snake(self):
+        # Initial positions for the snake segments
+        initial_positions = [(0, 0), (-20, 0), (-40, 0)]
+        for position in initial_positions:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        segment = turtle.Turtle("square")
+        segment.color("white")
+        segment.penup()
+        segment.goto(position)
+        self.segments.append(segment)
+
+    def move(self):
+        # Move each segment to the position of the previous segment, starting from the tail
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(20)
+
+    def up(self):
+        if self.direction != "down":
+            self.direction = "up"
+
+    def down(self):
+        if self.direction != "up":
+            self.direction = "down"
+
+    def left(self):
+        if self.direction != "right":
+            self.direction = "left"
+
+    def right(self):
+        if self.direction != "left":
+            self.direction = "right"
+
+    def change_direction(self):
+        if self.direction == "up":
+            self.head.setheading(90)
+        elif self.direction == "down":
+            self.head.setheading(270)
+        elif self.direction == "left":
+            self.head.setheading(180)
+        elif self.direction == "right":
+            self.head.setheading(0)
+
