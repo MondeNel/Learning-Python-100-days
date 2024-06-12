f785e2f7ddbd5e33e3eb66e1b188631aa42ab9f7
Monde Nel <Mondenel1996@gmail.com>
2024-06-12 14:27:41 +0200

    set up the screen play

--- Turtle_game/main.py
+++ Turtle_game/main.py
@@ -2,6 +2,7 @@
 import turtle
 import random
 
+# Define the RacingTurtle class
 class RacingTurtle:
     def __init__(self, color, y_position):
         self.turtle = turtle.Turtle()
@@ -15,3 +16,13 @@ class RacingTurtle:
 
     def get_x_position(self):
         return self.turtle.xcor()
+
+# Function to move the user-controlled turtle
+def user_move():
+    user_turtle.turtle.forward(10)
+
+
+# Set up the screen
+screen = turtle.Screen()
+screen.setup(width=500, height=400)
+screen.title("Turtle Race")
