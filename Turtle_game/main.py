201441ca6593b42f1320b0ea87794ff35811ad71
Monde Nel <Mondenel1996@gmail.com>
2024-06-12 14:12:40 +0200

    Added functions whichb will handle the movement of the turtle

--- Turtle_game/main.py
+++ Turtle_game/main.py
@@ -4,3 +4,24 @@ import turtle
 # Create a turtle object
 t = turtle.Turtle()
 t.speed(1)  # You can set the speed from 1 (slow) to 10 (fastest) or 0 (no animation)
+
+
+# Function to move the turtle forward
+def move_forward():
+    t.forward(20)
+
+# Function to move the turtle backward
+def move_backward():
+    t.backward(20)
+
+# Function to turn the turtle counterclockwise
+def turn_left():
+    t.left(15)
+
+# Function to turn the turtle clockwise
+def turn_right():
+    t.right(15)
+
+# Function to clear the drawing
+def clear_drawing():
+    t.clear()
