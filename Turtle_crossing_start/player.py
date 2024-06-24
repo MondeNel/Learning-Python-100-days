cedcc71bda6e68df1ece2a9d8ca2387bc3f85b71
Monde Nel <Mondenel1996@gmail.com>
2024-06-24 09:56:40 +0200

    Created a class for the Player which: presents the starting point of the payer. the finish line and also the moving method

--- Turtle_crossing_start/player.py
+++ Turtle_crossing_start/player.py
@@ -1,7 +1,28 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+class Player(Turtle):
+    """Class to represent the player-controlled turtle."""
+    
+    def __init__(self):
+        """Initialize the player at the starting position."""
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto_start()
+        self.setheading(90)
 
-class Player:
-    pass
+    def goto_start(self):
+        """Place the player turtle at the starting position."""
+        self.goto(STARTING_POSITION)
+
+    def move_up(self):
+        """Move the player turtle up by the MOVE_DISTANCE."""
+        self.forward(MOVE_DISTANCE)
+
+    def is_at_finish_line(self):
+        """Check if the player has reached the finish line."""
+        return self.ycor() > FINISH_LINE_Y
