f18892351ab4b67d457b572030a61332ed4175f9
Monde Nel <Mondenel1996@gmail.com>
2024-06-25 14:38:29 +0200

    Created my own DataFrame. Convert it to a csv file. getting the values from the data file

--- Weather_data/main.py
+++ Weather_data/main.py
@@ -33,13 +33,49 @@ temperatures = data["temp"] # day, temp, condition are SERIES
 # Calculate the average temperature
 average_temperature = temperatures.mean()
 
+#Calculate the maximum temperature
+max_temperature = temperatures.max()
+
+
 # Print the average temperature
 print(f"The average temperature is {average_temperature:.2f}°C")
 
-
-
+# Print the maximum temperature
+print(f"The maximum temperature is {max_temperature}°C")
 
 
 #Convert Data to a Dictionary
 data_dict = data.to_dict()
-# print(data_dict)
+
+
+
+
+
+# Create a DataFrame from scratch
+data_dict_students = {
+    "student": ["Amy", "James", "Siya"],
+    "grades": [89, 77, 50]
+}
+
+data_students = pd.DataFrame(data_dict_students)
+
+# Convert to a CSV file
+data_students.to_csv("students_data.csv", index=False)
+
+# Print the table of students with grades
+print(data_students)
+
+# Extract the grades
+grades = data_students["grades"]
+
+# Extract the student names
+student_names = data_students["student"]
+
+# Calculate the highest grade
+highest_grade = grades.max()
+
+# Find the student with the highest grade
+highest_grade_student = data_students[data_students["grades"] == highest_grade]["student"].iloc[0]
+
+# Print the highest grade and the corresponding student
+print(f"The highest grade is {highest_grade}% and it was obtained by {highest_grade_student}")
