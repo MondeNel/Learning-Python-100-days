94d83e662e964342cca1195ed52162453f53a922
Monde Nel <Mondenel1996@gmail.com>
2024-06-25 15:34:37 +0200

    Created a Dictionary. Converted it into a DataFrame. Converted the DataFrame into a CSV file

--- Squirrel_data/main.py
+++ Squirrel_data/main.py
@@ -3,9 +3,22 @@ import pandas as pd
 # Read the CSV file using pandas
 data = pd.read_csv("2018_Central_Park_Squirrel_Census.csv")
 
-# Filter the data to find the length of the gray squirrels
+# Filter the data to find the length of the squirrels
 gray_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels = len(data[data["Primary Fur Color"] == "Black"])
 
-# Print the filtered data
-print(gray_squirrels)
+#Create a Dictionary of the data
+data_dict = {
+    "Fur Color" : ["Gray", "Cinnamon", "Black"],
+    "Count" : [gray_squirrels, red_squirrels, black_squirrels]
+}
 
+#Convert into a DataFrame
+data_squirrels = pd.DataFrame(data_dict)
+
+# Print the data frame
+print(data_squirrels)
+
+#Convert to CSV file
+data_squirrels.to_csv("squirrel_count.csv")
