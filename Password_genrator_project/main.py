93f368029ced77b9145aeeec43fe5abc4882a1e7
Monde Nel <Mondenel1996@gmail.com>
2024-07-10 12:45:02 +0200

    added the entries for their respective placeholders

--- Password_genrator_project/main.py
+++ Password_genrator_project/main.py
@@ -25,6 +25,16 @@ email_label.grid(column=0, row=2)
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
+# Entries
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "example@example.com")
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+
 
 
 
